6b599f9c1644e66bde1281b6416b5a372aee7188
mickael baratier <mickael.baratier@le-campus-numerique.fr>
2022-05-02 12:22:18 +0200

    add the code of the header, but i don't have footer on my web site

--- header.php
+++ header.php
@@ -1,1 +1,65 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>page accueil</title>
+</head>
+
+<body>
+<!-- navbar -->
+<nav class="navbar navbar-expand-md navbar-light" id="navbar">
+    <div class="container-xxl">
+        <a href="index.html" class="navbar-brand">
+            <img src="images/logo.png" alt="Logo retour acceuil">
+        </a>
+        <!-- button -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav"
+                aria-controls="main-nav" aria-expanded="false" aria-label="toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- navbar links -->
+        <div class="collapse navbar-collapse" id="main-nav">
+            <ul class="navbar-nav justify-content-around col-12 text-size-50">
+                <li class="nav-item" id="link">
+                    <a href="index.html" class="nav-link d-flex flex-column flex-md-row align-items-center">
+                        <img src="images/logo.png" alt="logo" class="logo_link">
+                        <h3 class="nav-title">Accueil</h3>
+                    </a>
+                </li>
+                <svg class="svg_equipe" width="5" height="60">
+                    <line x1="0" y1="0" x2="0" y2="110" stroke="#459DCA" stroke-width="3"></line>
+                </svg>
+                <li class="nav-item" id="link">
+                    <a href="comparatif.html" class="nav-link d-flex flex-column flex-md-row  align-items-center">
+                        <img src="images/logo.png" alt="logo" class="logo_link">
+                        <h3 class="nav-title">Service</h3>
+                    </a>
+                </li>
+                <svg class="svg_equipe" width="5" height="60">
+                    <line x1="0" y1="0" x2="0" y2="110" stroke="#459DCA" stroke-width="3"></line>
+                </svg>
+                <li class="nav-item" id="link">
+                    <a href="equipe.html" class="nav-link d-flex flex-column flex-md-row  align-items-center">
+                        <img src="images/logo.png" alt="logo" class="logo_link">
+                        <h3 class="nav-title">L'équipe</h3>
+                    </a>
+                </li>
+                <svg class="svg_equipe" width="5" height="60">
+                    <line x1="0" y1="0" x2="0" y2="110" stroke="#459DCA" stroke-width="3"></line>
+                </svg>
+                <li class="nav-item" id="link">
+                    <a href="contact.html" class="nav-link d-flex flex-column flex-md-row  align-items-center">
+                        <img src="images/logo.png" alt="logo" class="logo_link">
+                        <h3 class="nav-title">Contact</h3>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
