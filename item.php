7ea07be63b67e8741c24a9fe49e4c249eb29b295
mickael baratier <mickael.baratier@le-campus-numerique.fr>
2022-05-02 13:25:30 +0200

    add design of web site

--- item.php
+++ item.php
@@ -1,14 +1,62 @@
 <?php
 include 'header.php';
-
-$name = "Création de site web";
-$price = 1000;
-$address = "image/icone_multi_tab.png";
-
-echo $name . " : ";
 ?>
-<img src="<?= $address ?>" alt="image du produit">
-<?php
-print_r("\n" . "l'article coute " . $price . "€");
-?>
+<div class="container-xl accueil">
+        <div class="row g-0 bg-blue text-justify">
+            <div class="col">
+                <img src="image/Web Dev-Blog.png" class="img-fluid titi" alt="dessin ordinateur">
+            </div>
+            <div class="col">
+                <div>
+                    <h1>WELCOME</h1>
+                    <p>
+Vous souhaitez développer votre notoriété et valoriser en ligne votre entreprise,
+                        les produits et services que vous commercialisez afin d'engendrer de nouveaux contacts et
+                        prospects, générer des demandes de devis ?<br></p>
+                    <p> Nous sommes source de propositions,
+                        vous accompagnons et créons votre site web de présentation d'activité (site internet
+                        institutionnel,présentation de catalogue produits et services)
+                        afin qu'il soit le reflet de l'image de votre entreprise et dispose des fonctionnalités
+                        dont vous avez fait état dans votre définition de besoin (cahier de charges).
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-xl accueil">
+        <div class="row flex-wrap justify-content-between custom-line text-center gneh ">
+            <div class="col-3 bowl bg-primary">
+                <img src="image\icone_multi_tab.png" class="rounded float-start img-fluid  toto" alt="icone_multi_tab">
+                <h2 class="hi">Création de site web</h2>
+                <div class="tata w-100">
+                    <a href="comparatif.html" class="hoho"><p>plus de détails..</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-3 bowl bg-danger">
+                <img src="image\icone_int.png" class="rounded float-start img-fluid  toto" alt="icone_internet">
+                <h2 class="hi">Gestion Hebergeur</h2>
+                <div class="tata w-100">
+                    <a href="comparatif.html" class="hoho">
+                        <p>plus de détails..</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-3 bowl bg-warning">
+                <img src="image\283403-200.png" class="rounded float-start img-fluid toto"
+                    alt="icone_graphique_hausse">
+                <h2 class="hi">Formule complète</h2>
+                <div class="tata w-100">
+                    <a href="comparatif.html" class="hoho">
+                        <p>plus de détails..</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</body>
 
+</html>
