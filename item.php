ee6344badd3d960bed9270034fa140e4f408a0dd
mickael baratier <mickael.baratier@le-campus-numerique.fr>
2022-05-02 13:36:44 +0200

    add update in item.php for include footer

--- item.php
+++ item.php
@@ -58,5 +58,6 @@ include 'header.php';
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
 </body>
-
-</html>
+<?php
+include 'footer.php';
+?>
