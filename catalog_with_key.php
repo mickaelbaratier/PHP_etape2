75cba4c64767e2c22aecdc5334c2ec754b369aec
mickael baratier <mickael.baratier@le-campus-numerique.fr>
2022-05-03 16:56:14 +0200

    partie 4 c'est le soir je m'arrete. a faire demain : continuer la lecture du tableau pour l'affichage.

---
+++ catalog_with_key.php
@@ -0,0 +1,22 @@
+<?php
+$creation_site_web = [
+    "name" => "creation de site web",
+    "price" => 2500,
+    "weight" => 0,
+    "discount" => 10,
+    "picture_url" => "image\icone_multi_tab.png",
+];
+$formule_complette = [
+    "name" => "formule complette",
+    "price" => 10000,
+    "weight" => 0,
+    "discount" => 5,
+    "picture_url" => "image\\283403-200.png",
+];
+$gestion_hebergeur = [
+    "name" => "gestion hebergeur",
+    "price" => 5000,
+    "weight" => 0,
+    "discount" => 0,
+    "picture_url" => "image\icone_int.png",
+];
