6da9631fa8ba6eda7146c2a72727d57d0f549887
mickael baratier <mickael.baratier@le-campus-numerique.fr>
2022-05-05 11:13:47 +0200

    partie 5 : changement du prix en centime.

--- multidimentional_catalog.php
+++ multidimentional_catalog.php
@@ -2,21 +2,21 @@
 $product = [
     "creation_site_web" => [
         "name" => "creation de site web",
-        "price" => 2500,
+        "price" => 250000,
         "weight" => 0,
         "discount" => 10,
         "picture_url" => "image\icone_multi_tab.png",
     ],
     "formule_complette" => [
         "name" => "gestion hebergeur",
-        "price" => 5000,
+        "price" => 500000,
         "weight" => 0,
         "discount" => 0,
         "picture_url" => "image\icone_int.png",
     ],
     "gestion_hebergeur" => [
         "name" => "formule complette",
-        "price" => 10000,
+        "price" => 1000000,
         "weight" => 0,
         "discount" => 5,
         "picture_url" => "image\\283403-200.png",
