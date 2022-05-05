c20ba4b1b4932520d6f081b4a566670b81a13b4f
mickael baratier <mickael.baratier@le-campus-numerique.fr>
2022-05-05 11:28:04 +0200

    partie 5 :   5.1 : MAJ pour ajouter la fonction calcul du prix avec sepparateur des milliers et aussi l'ajout du separateur des centimes.

--- my-function.php
+++ my-function.php
@@ -5,11 +5,17 @@ function print_catalog($product){
         <div>
             <h2><?php echo $product[$part]["name"] ?> </h2>
             <p>
-                Prix pour une <?php echo $product[$part]["name"] ?> : <?php echo $product[$part]["price"] ?> €.
+                Prix pour une <?php echo $product[$part]["name"] ?> : <?php echo formatPrice($product[$part]["price"]) ?> €.
                 <br>Avec <?php echo $product[$part]["discount"] ?> % de reduction.
             </p>
             <img src="<?php echo $product[$part]["picture_url"] ?>" alt="<?php echo $product[$part]["name"] ?>">
         </div>
         <?php
     }
+}
+function formatPrice($prix){
+    //$prix = floatval($prix);
+    $prix = $prix/100;
+    $prix = number_format($prix, 2,',',' ');
+    return $prix;
 }
